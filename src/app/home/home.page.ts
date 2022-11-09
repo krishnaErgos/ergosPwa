a65464bc122122793d3c89521d652d7a2bb6976c
krishna <krishnaraj.n@ergos.in>
2022-11-09 15:10:44 +0530

    Initial commit.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { Share } from '@capacitor/share';
+// import { BLE } from '@awesome-cordova-plugins/ble/ngx';
+import { BleClient, BluetoothLe } from '@capacitor-community/bluetooth-le';
+
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,81 @@ export class HomePage {
   myImage = '';
   // eslint-disable-next-line @typescript-eslint/type-annotation-spacing
   position:any;
-  constructor() {}
+  devices: any[] = [];
+  ble = false;
+  scanText = '';
+  constructor(private change: ChangeDetectorRef) {
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit(){
+    BleClient.initialize().then(() => {
+      BleClient.isEnabled().then((res)=> {
+        console.log(res);
+        if(res) {
+          this.ble = true;
+        } else {
+          this.ble = false;
+        }
+
+      });
+    });
+  };
+
+  toggleBle(event){
+    if(this.ble) {
+      this.enableBluetooth();
+    } else {
+      this.disableBluetooth();
+    }
+  }
+  enableBluetooth() {
+    BleClient.enable();
+  }
+
+  disableBluetooth(){
+    BleClient.disable();
+  }
+
+  startScanning(){
+    this.scanText = 'Scanning...';
+    BleClient.requestLEScan({allowDuplicates : false}, (res1) => {
+      console.log(res1);
+      if(res1.localName) {
+        this.devices.push(res1);
+        this.change.detectChanges();
+      }
+      console.log(this.devices);
+    });
+    setTimeout(() => {
+      console.log('Hi');
+      console.log(this.devices);
+      this.stopScanning();
+    },20000);
+  }
+
+  stopScanning(){
+    BluetoothLe.stopLEScan().then(() => {
+      this.scanText = '';
+    });
+  }
+  connect(device, index){
+    BleClient.connect(device.device.deviceId).then(() => {
+      this.devices[index].connection = true;
+      this.change.detectChanges();
+      alert('Connected');
+    }, (err) => {
+      alert(err);
+    });
+  }
+
+  disconnect(device, index) {
+    BleClient.connect(device.device.deviceId).then(() => {
+      this.devices[index].connection = false;
+      this.change.detectChanges();
+      alert('Disconnected');
+    });
+  }
 
   async captureImage() {
     const image = await Camera.getPhoto({
@@ -25,6 +101,7 @@ export class HomePage {
     });
 
     if (image) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       this.myImage = `data:image/jpeg;base64,${image.base64String}`!;
     }
   }
@@ -44,5 +121,7 @@ export class HomePage {
       url: 'http://ionicacademy.com/'
     });
   }
+
+
 
 }
